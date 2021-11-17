0eb03b415745de81099d27a6140533d828b0dd7d
Osama Dahnoun <omdahnou@ncsu.edu>
2021-11-16 23:05:12 -0500

    App statically adds top ten songs to page

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+var submitBtn = document.querySelector('#submitBtn');
+var container = document.querySelector('#container');
+
+var Song1El = document.querySelector('#song1');
+var Song2El = document.querySelector('#song2');
+var Song3El = document.querySelector('#song3');
+var Song4El = document.querySelector('#song4');
+var Song5El = document.querySelector('#song5');
+var Song6El = document.querySelector('#song6');
+var Song7El = document.querySelector('#song7');
+var Song8El = document.querySelector('#song8');
+var Song9El = document.querySelector('#song9');
+var Song10El = document.querySelector('#song10');
+
+var getArtistSongs = function(firstName) {
+    var apiUrl = 'https://theaudiodb.p.rapidapi.com/track-top10.php?s=' + firstName
+    fetch(apiUrl, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "theaudiodb.p.rapidapi.com",
+		"x-rapidapi-key": "ef46e6a41amshf3c1a1224464392p1c3b64jsnd05e7e63b5ee"
+	}
+})
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                var song1 = data.track[0].strTrack
+                Song1El.textContent = song1
+                var song2 = data.track[1].strTrack
+                Song2El.textContent = song2
+                var song3 = data.track[2].strTrack
+                Song3El.textContent = song3
+                var song4 = data.track[3].strTrack
+                Song4El.textContent = song4
+                var song5 = data.track[4].strTrack
+                Song5El.textContent = song5
+                var song6 = data.track[5].strTrack
+                Song6El.textContent = song6
+                var song7 = data.track[6].strTrack
+                Song7El.textContent = song7
+                var song8 = data.track[7].strTrack
+                Song8El.textContent = song8
+                var song9 = data.track[8].strTrack
+                Song9El.textContent = song9
+                var song10 = data.track[9].strTrack
+                Song10El.textContent = song10
+    })
+        }
+    }) 
+}
+getArtistSongs('drake');
